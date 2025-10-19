5b29cacae63f1fdc36b1ebe5ffa5de0419e636aa
vipwhy12 <vipwhy12@naver.com>
2025-10-19 17:37:31 +0900

    first commit

---
+++ src/main/kotlin/org/example/optitrip/OptiTripApplication.kt
@@ -0,0 +1,11 @@
+package org.example.optitrip
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OptiTripApplication
+
+fun main(args: Array<String>) {
+    runApplication<OptiTripApplication>(*args)
+}
